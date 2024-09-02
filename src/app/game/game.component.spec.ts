c7572a3b43c4e4cc51fe4373ec19a5b45f1ff713
Mikolaj <mikolaj.porada@icloud.com>
2024-09-02 15:36:49 +0200

    add UT

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,23 +1,103 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GameComponent } from "./game.component";
+import { SwapiService } from "../services/swapi.service";
+import { of } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Card } from "../interfaces/card.interface";
 
-import { GameComponent } from './game.component';
+const mockLeftCard: Card = {
+  name: "Luke Skywalker",
+  mass: "77",
+  crew: "1",
+};
 
-describe('GameComponent', () => {
+const mockRightCard: Card = {
+  name: "Darth Vader",
+  mass: "136",
+  crew: "1",
+};
+
+describe("GameComponent", () => {
   let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+  let mockSwapiService: jasmine.SpyObj<SwapiService>;
+  let mockSnackBar: jasmine.SpyObj<MatSnackBar>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    mockSwapiService = jasmine.createSpyObj("SwapiService", ["getResource"]);
+    mockSnackBar = jasmine.createSpyObj("MatSnackBar", ["open"]);
 
-    fixture = TestBed.createComponent(GameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new GameComponent(mockSwapiService);
+
+    mockSwapiService.getResource.and.returnValues(
+      of({ result: { properties: mockLeftCard } }),
+      of({ result: { properties: mockRightCard } })
+    );
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should fetch cards and determine winner based on mass", () => {
+    component.startGame();
+
+    expect(component.leftCard).toEqual(mockLeftCard);
+    expect(component.rightCard).toEqual(mockRightCard);
+
+    component.determineWinner("people");
+
+    expect(component.winner).toBe("Right Player");
+  });
+
+  it("should fetch cards and determine winner based on crew", () => {
+    component.startGame();
+
+    expect(component.leftCard).toEqual(mockLeftCard);
+    expect(component.rightCard).toEqual(mockRightCard);
+
+    component.determineWinner("starships");
+
+    expect(component.winner).toBe("Draw");
+  });
+
+  it("should handle unknown values and notify user", () => {
+    const unknownCard: Card = {
+      name: "Unknown",
+      mass: "unknown",
+      crew: "unknown",
+    };
+
+    mockSwapiService.getResource.and.returnValues(
+      of({ result: { properties: unknownCard } }),
+      of({ result: { properties: mockRightCard } })
+    );
+
+    component.startGame();
+
+    expect(component.leftCard).toEqual(unknownCard);
+    expect(component.rightCard).toEqual(mockRightCard);
+
+    component.determineWinner("people");
+
+    expect(component.winner).toBe("Cannot compare due to unknown values");
+  });
+
+  it("should update scores correctly", () => {
+    component.updateScore("Left Player");
+    expect(component.leftScore).toBe(1);
+    expect(component.rightScore).toBe(0);
+
+    component.updateScore("Right Player");
+    expect(component.leftScore).toBe(1);
+    expect(component.rightScore).toBe(1);
+
+    component.updateScore("Draw");
+    expect(component.leftScore).toBe(1);
+    expect(component.rightScore).toBe(1);
+  });
+
+  it("should parse numbers correctly", () => {
+    expect(component["parseNumber"]("1,000")).toBe(1000);
+    expect(component["parseNumber"]("123")).toBe(123);
+    expect(component["parseNumber"]("unknown")).toBeNaN();
   });
 });
--- src/app/services/swapi.service.spec.ts
+++ src/app/services/swapi.service.spec.ts
@@ -1,16 +1,108 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from "@angular/common/http";
+import { of, throwError } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { SwapiService } from "./swapi.service";
 
-import { SwapiService } from './swapi.service';
-
-describe('SwapiService', () => {
+describe("SwapiService", () => {
   let service: SwapiService;
+  let mockHttp: jasmine.SpyObj<HttpClient>;
+  let mockSnackBar: jasmine.SpyObj<MatSnackBar>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SwapiService);
+    mockHttp = jasmine.createSpyObj("HttpClient", ["get"]);
+    mockSnackBar = jasmine.createSpyObj("MatSnackBar", ["open"]);
+
+    service = new SwapiService(mockHttp, mockSnackBar);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  describe("getRandomId", () => {
+    it("should return a specific ID when mocked", () => {
+      spyOn<any>(service, "getRandomId").and.returnValue(5);
+
+      const resource = "people";
+      const mockResponse = { result: { properties: {} } };
+      mockHttp.get.and.returnValue(of(mockResponse));
+
+      service.getResource(resource).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      expect(mockHttp.get).toHaveBeenCalledWith(
+        `https://www.swapi.tech/api/${resource}/5`
+      );
+    });
+  });
+
+  describe("retryRequest", () => {
+    beforeEach(() => {
+      spyOn<any>(service, "getRandomId").and.returnValue(1);
+    });
+
+    it("should handle a successful request", () => {
+      const mockResponse = { result: { properties: {} } };
+      const resource = "people";
+
+      mockHttp.get.and.returnValue(of(mockResponse));
+
+      service.getResource(resource).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+
+      expect(mockHttp.get).toHaveBeenCalledWith(
+        `https://www.swapi.tech/api/${resource}/1`
+      );
+    });
+
+    it("should handle an error and display a snackbar", () => {
+      const error = new ErrorEvent("Network error");
+      const resource = "people";
+
+      mockHttp.get.and.returnValue(throwError(() => error));
+
+      service.getResource(resource).subscribe({
+        error: () => {
+          expect(mockSnackBar.open).toHaveBeenCalledWith(
+            "Request failed, please try again.",
+            "Close",
+            {
+              duration: 3000,
+              horizontalPosition: "center",
+              verticalPosition: "top",
+            }
+          );
+        },
+      });
+
+      expect(mockHttp.get).toHaveBeenCalledWith(
+        `https://www.swapi.tech/api/${resource}/1`
+      );
+    });
+  });
+
+  describe("handleError", () => {
+    it("should display an error message and return an observable with an error message", () => {
+      const error = new ErrorEvent("Network error") as any;
+
+      spyOn(console, "error");
+
+      service["handleError"](error).subscribe({
+        error: (err) => {
+          expect(err).toBe("Something bad happened, please try again later.");
+          expect(mockSnackBar.open).toHaveBeenCalledWith(
+            "Request failed, please try again.",
+            "Close",
+            {
+              duration: 3000,
+              horizontalPosition: "center",
+              verticalPosition: "top",
+            }
+          );
+        },
+      });
+    });
   });
 });
